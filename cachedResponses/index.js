07624197f2a79456a8f0cc4457b0a502f1928424
Alexey Zorkaltsev <alexey@zorkaltsev.com>
2017-09-09 18:52:48 +0300

    refactoring

--- cachedResponses/index.js
+++ cachedResponses/index.js
@@ -1,6 +1,6 @@
 import throwIfMissing from 'throw-if-missing'
 
-import Service from '../services'
+import {validateAndCopyOptionsFactory, validateArgumentNameOptions, VType} from '../validation'
 
 const debug = require('debug')('cached_response');
 
@@ -8,180 +8,153 @@ export const REUSE_RESULT_TIME = 3 * 60 * 1000;
 export const REUSE_RESULT_ON_ERROR_TIME = 1 * 60 * 1000;
 export const MAX_PARALLEL_REQUESTS = 3;
 
-export function CachedResponsesService(services) {
+const validateOptions = validateAndCopyOptionsFactory({
+  reuseResultTime: {type: VType.Int().zero().positive(), copy: true},
+  reuseResultOnErrorTime: {type: VType.Int().zero().positive(), copy: true},
+  maxParallelRequests: {type: VType.Int().zero().positive(), copy: true},
+  now: {type: VType.Function(), copy: true}, // для тестирование - устаревший подход
+});
+
+/**
+ * Базовый класс для сервисов, которые получают данные по ключу (коду) из внешних систем.  И эти данные можно повторно
+ * использовать в течении некоторого времени, без повторных запросов к внешнему сервису.  Так же, чтобы не перегружать
+ * внешний сервис, возможно ограничение по количеству одновременных запросов к внешнему сервису.
+ */
+export default class CachedResponsesService {
+
+  _goCount = 0;
+  _goQueue = [];
+
+  _cache = {};
+
+  _reuseResultTime = REUSE_RESULT_TIME;
+  _reuseResultOnErrorTime = REUSE_RESULT_ON_ERROR_TIME;
+  _maxParallelRequests = MAX_PARALLEL_REQUESTS;
+
+  _now = function () {
+    (new Date()).getTime();
+  };
+
+  constructor(options) {
+    validateOptions(options, {argument: 'options', copyTo: this});
+  }
 
   /**
-   * Базовый класс для сервисов, которые получают данные по ключу (коду) из внешних систем.  И эти данные можно повторно
-   * использовать в течении некоторого времени, без повторных запросов к внешнему сервису.  Так же, чтобы не перегружать
-   * внешний сервис, возможно ограничение по количеству одновременных запросов к внешнему сервису.
+   * Смотрит есть ли уже данные в кеш и не прошло ли время, когда надо их обновить.  Если нужно обновить запускает загрузку
+   * через функцию getData().  Если функция возвращается с ошибкой - то используется предыдущее значение.  Если загрузка уже
+   * началась в другом методе getDataFromCache, то очередная загрузка не стартует, а запрос встает в очередь на ожидание ответа
+   * от чужого запроса - promiseValue.await.
+   *
+   * @returns {Promise}
+   * @private
    */
-  class CachedResponsesService {
-
-    _goCount = 0;
-    _goQueue = [];
-
-    _cache = {};
-
-    _reuseResultTime = REUSE_RESULT_TIME;
-    _reuseResultOnErrorTime = REUSE_RESULT_ON_ERROR_TIME;
-    _maxParallelRequests = MAX_PARALLEL_REQUESTS;
-
-    _now = function () {
-      (new Date()).getTime();
-    };
-
-    constructor() {
-      if (arguments.length > 0) throw new Error('Arguments are not expected')
-      super();
+  _find /* async */(args = throwIfMissing('args')) {
+    debug(`_find(%s)`, args);
+    const key = this._argsToKey(args);
+    let cachedValue = this._cache[key];
+    if (cachedValue) {
+      if (cachedValue.goodTill >= this._now()) {
+        debug('return cached value');
+        return Promise.resolve(cachedValue.data);
+      }
+    } else {
+      debug('new cache value');
+      this._cache[key] = cachedValue = {key};
     }
-
-    async _init(options) {
-      if (arguments.length > 0) {
-        if (!(typeof options === 'object' && options != null && !Array.isArray(options)))
-          throw new Error(`Invalid argument 'options'`);
-        for (let optName in options) {
-          switch (optName) {
-            case 'reuseResultTime':
-              this._reuseResultTime = options[optName];
-              break;
-            case 'reuseResultOnErrorTime':
-              this._reuseResultOnErrorTime = options[optName];
-              break;
-            case 'maxParallelRequests':
-              this._maxParallelRequests = options[optName];
-              break;
-            case 'now':
-              this._now = options[optName];
-              break;
-            default:
-              throw new Error(`Unexpected option: ${optName}`);
+    return new Promise((resolve, reject) => {
+      if (!cachedValue.hasOwnProperty('await')) {
+        debug('queue loading');
+        cachedValue.await = this._loadDataQueue(args).then((data) => {
+          debug('load succeeded', data);
+          delete cachedValue.await;
+          cachedValue.data = data; // Сохраняем новое значение
+          cachedValue.goodTill = this._now() + this._reuseResultTime;
+          return data;
+        }, (err) => {
+          debug('load failed: %O', err);
+          delete cachedValue.await;
+          if (cachedValue.hasOwnProperty('data')) {
+            cachedValue.goodTill = this._now() + this._reuseResultOnErrorTime;
+            return cachedValue.data; // Используем старое значение
           }
-        }
-      }
-
-      super._init();
-    }
-
-    /**
-     * Смотрит есть ли уже данные в кеш и не прошло ли время, когда надо их обновить.  Если нужно обновить запускает загрузку
-     * через функцию getData().  Если функция возвращается с ошибкой - то используется предыдущее значение.  Если загрузка уже
-     * началась в другом методе getDataFromCache, то очередная загрузка не стартует, а запрос встает в очередь на ожидание ответа
-     * от чужого запроса - promiseValue.await.
-     *
-     * @returns {Promise}
-     * @private
-     */
-    _find /* async */(args = throwIfMissing('args')) {
-      debug(`_find(%s)`, args);
-      const key = this._argsToKey(args);
-      let cachedValue = this._cache[key];
-      if (cachedValue) {
-        if (cachedValue.goodTill >= this._now()) {
-          debug('return cached value');
-          return Promise.resolve(cachedValue.data);
-        }
-      } else {
-        debug('new cache value');
-        this._cache[key] = cachedValue = {key};
-      }
-      return new Promise((resolve, reject) => {
-        if (!cachedValue.hasOwnProperty('await')) {
-          debug('queue loading');
-          cachedValue.await = this._loadDataQueue(args).then((data) => {
-            debug('load succeeded', data);
-            delete cachedValue.await;
-            cachedValue.data = data; // Сохраняем новое значение
-            cachedValue.goodTill = this._now() + this._reuseResultTime;
-            return data;
-          }, (err) => {
-            debug('load failed: %O', err);
-            delete cachedValue.await;
-            if (cachedValue.hasOwnProperty('data')) {
-              cachedValue.goodTill = this._now() + this._reuseResultOnErrorTime;
-              return cachedValue.data; // Используем старое значение
-            }
-            return null;
-          });
-        }
-        resolve(cachedValue.await);
-      });
-    }
-
-    /**
-     * Метод, который запускает следующий в очереди go метод, вне зависимости от успешного или не успешного завершения предыдущего шага.
-     */
-    _nextGo = (data) => {
-      debug('nextGo');
-      this._goCount--;
-      let next = this._goQueue.shift();
-      if (next) next();
-      return data;
-    };
-
-    _nextGoOnReject = (err) => {
-      debug('nextGo');
-      this._goCount--;
-      let next = this._goQueue.shift();
-      if (next) next();
-      return Promise.reject(err);
-    };
-
-    /**
-     * Запускает процесс загрузки данных.  Если есть ограничение по кличеству одновременных операций по загрузке, то ставит
-     * очередную загрузку в очередь.
-     *
-     * @param args Произвольный map аргументов, для операции загрузки.
-     * @returns {Promise}
-     * @private
-     */
-    _loadDataQueue(args) {
-
-      if (this._maxParallelRequests < 1) { // нет ограничеия по количеству одновременных загрузок
-
-        return this._load(args);
-
-      } else {
-
-        // всегда возвращает Promise соотвествующий аргументам.  При этом, реальная операция загрузки может попасть в очередь
-        return new Promise((resolve, reject) => {
-          // метод который запускает реальный процесс загрузки и связывает результат с ранее возвращенным Promise для данных аргументов
-          let go = () => {
-            debug('go');
-            this._goCount++;
-            resolve(this._load(args).then(this._nextGo, this._nextGoOnReject));
-          };
-
-          if (this._goCount === this._maxParallelRequests)
-            this._goQueue.push(go); // в очередь
-          else
-            go(); // сразу выполняем
+          return null;
         });
       }
-    }
+      resolve(cachedValue.await);
+    });
+  }
 
-    /**
-     * Формирует ключ для элемента кеширования, для данных аргументов.
-     *
-     * @param args
-     * @private
-     */
-    _argsToKey(args = throwIfMissing('args')) {
-      return JSON.stringify(args);
-    }
+  /**
+   * Метод, который запускает следующий в очереди go метод, вне зависимости от успешного или не успешного завершения предыдущего шага.
+   */
+  _nextGo = (data) => {
+    debug('nextGo');
+    this._goCount--;
+    let next = this._goQueue.shift();
+    if (next) next();
+    return data;
+  };
 
-    /**
-     * Класс наследник должен переопределить метод, чтоб он загружал данные, в соответствии с переданными аргументами.
-     *
-     * Даже если данных для данных параметров нет, должен возвращаться пустой результат (не ошибка).
-     *
-     * @returns {Promise}
-     * @private
-     */
-    async _load(args) {
-      throw new Error('not implemented')
+  _nextGoOnReject = (err) => {
+    debug('nextGo');
+    this._goCount--;
+    let next = this._goQueue.shift();
+    if (next) next();
+    return Promise.reject(err);
+  };
+
+  /**
+   * Запускает процесс загрузки данных.  Если есть ограничение по кличеству одновременных операций по загрузке, то ставит
+   * очередную загрузку в очередь.
+   *
+   * @param args Произвольный map аргументов, для операции загрузки.
+   * @returns {Promise}
+   * @private
+   */
+  _loadDataQueue(args) {
+
+    if (this._maxParallelRequests < 1) { // нет ограничеия по количеству одновременных загрузок
+
+      return this._load(args);
+
+    } else {
+
+      // всегда возвращает Promise соотвествующий аргументам.  При этом, реальная операция загрузки может попасть в очередь
+      return new Promise((resolve, reject) => {
+        // метод который запускает реальный процесс загрузки и связывает результат с ранее возвращенным Promise для данных аргументов
+        let go = () => {
+          debug('go');
+          this._goCount++;
+          resolve(this._load(args).then(this._nextGo, this._nextGoOnReject));
+        };
+
+        if (this._goCount === this._maxParallelRequests)
+          this._goQueue.push(go); // в очередь
+        else
+          go(); // сразу выполняем
+      });
     }
   }
 
-  return new (require('../../common/services').Service(services)(Cyberlines))(name, configAPI.get('Ciberlines'));
-  return Service(CachedResponsesService);
+  /**
+   * Формирует ключ для элемента кеширования, для данных аргументов.
+   *
+   * @param args
+   * @private
+   */
+  _argsToKey(args = throwIfMissing('args')) {
+    return JSON.stringify(args);
+  }
+
+  /**
+   * Класс наследник должен переопределить метод, чтоб он загружал данные, в соответствии с переданными аргументами.
+   *
+   * Даже если данных для данных параметров нет, должен возвращаться пустой результат (не ошибка).
+   *
+   * @returns {Promise}
+   * @private
+   */
+  async _load(args) {
+    throw new Error('not implemented')
+  }
 }
