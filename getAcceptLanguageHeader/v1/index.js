c6aacaa279c738c8d94fbf5f43795dcdd77862a6
Alexey Zorkaltsev <alexey@zorkaltsev.com>
2017-10-17 20:58:52 +0300

    work in progress

--- getAcceptLanguageHeader/v1/index.js
+++ getAcceptLanguageHeader/v1/index.js
@@ -8,11 +8,13 @@ export default oncePerServices(function (services) {
     parentLevelBuilder.addQuery({
       name: 'supportedLocales',
       description: `Список locale, который поддреживает клиент`,
+      args: `supportedLocales: [String!]`,
       type: `String!`,
       resolver: async function (obj, {supportedLocales}, context) {
         acceptLanguage.languages(supportedLocales);
+        const req = context.request;
         const locale = acceptLanguage.get(req.headers && req.headers['accept-language']);
-        return {locale};
+        return locale;
       }
     });
   }
