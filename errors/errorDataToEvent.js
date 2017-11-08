34a9a655e718147267dc44493af4ee043ae8af5a
Alexey Zorkaltsev <alexey@zorkaltsev.com>
2017-11-08 12:21:23 +0300

    work in progress

--- errors/errorDataToEvent.js
+++ errors/errorDataToEvent.js
@@ -5,7 +5,7 @@ import reduceErrorStack from '../utils/reduceErrorStack'
  * Заполняет сообщение для шины, на основе данных объекта типа Error.
  */
 export default function errorDataToEvent(error, event) {
-  const message = event.errorMessage = buildFullErrorMessage(error); // stack и context в полное название не включаются
+  const message = event.errorMessage = buildFullErrorMessage(error);
   event.errorStack = reduceErrorStack(error, message);
   if (hasOwnProperty.call(error, 'context')) event.context = error.context;
 };
