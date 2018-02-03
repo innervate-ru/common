703d1721a3677ee9f2baab7e25d719c22f044be6
Alexey Zorkaltsev <alexey@zorkaltsev.com>
2018-02-03 13:11:22 +0300

    work in progress

--- events/BaseEvent.js
+++ events/BaseEvent.js
@@ -6,7 +6,7 @@ export default validateEventFactory({
   node: {type: VType.String()}, // не required, так как подставляется автоматически в Bus.<log>() - название хоста, на котором выполняется код
   message: {type: VType.String()}, // поле, которое заполняется значение toString(), если оно указано для события
   type: {required: true, type: VType.String().notEmpty()}, // event, error, warning ...
-  service: {required: true, type: VType.String(), validate: v => /^[\w\:\-\_\/]+$/.test(v)}, // название ноды и сервиса, через двоеточие
+  service: {required: true, type: VType.String(), validate: v => /^[\w\:\-\_\/\.]+$/.test(v)}, // название ноды и сервиса, через двоеточие
   context: require('../context/context.schema').contextSchema, // контекст, в котором было созданное событие
   _final: false,
 });
