843a977b2babaa983ce255077675668feb35b607
Alexey Zorkaltsev <alexey@zorkaltsev.com>
2018-04-03 16:49:42 +0300

    work in progress

--- errors/error.schema.js
+++ errors/error.schema.js
@@ -3,5 +3,7 @@ import {VType} from '../validation'
 export const eventErrorSchema = {
   message: {required: true, type: VType.String().notEmpty()},
   stack: {type: VType.String().notEmpty()}, // стандартный call-stack из объекта Error, желательно, уменьшенный за счет выкидывания строк системного кода
+  context: {type: VType.String()},
+  calls: {type: VType.Array()},
   _final: true,
 };
