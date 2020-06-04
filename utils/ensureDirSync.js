7c30cebcc1cbeb857674dfdd87aa932bb4f22f05
Alexey Zorkaltsev <alexey@zorkaltsev.com>
2020-06-04 08:52:19 +0300

    wip

---
+++ utils/ensureDirSync.js
@@ -0,0 +1,22 @@
+import fs from 'fs'
+import path from 'path'
+import {promisify} from 'util'
+
+import {missingArgument} from '../validation/arguments'
+
+const fsStat = promisify(fs.stat);
+const fsMkdir = promisify(fs.mkdir);
+
+export default async function (dir = missingArgument('dir')) {
+  dir = path.resolve(process.cwd(), dir);
+  try {
+    if (!fs.statSync(dir).isDirectory())
+      throw new Error(`'${dir}' is not a directory`);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      fs.mkdirSync(dir);
+    }
+    else throw err;
+  }
+  return dir;
+}
