283202be9f2421ddb9f5753150b41e6f4d25dd30
Alexey Zorkaltsev <alexey@zorkaltsev.com>
2020-08-13 18:25:46 +0300

    wip

--- _samples/asyncHttpAction.js
+++ _samples/asyncHttpAction.js
@@ -4,20 +4,20 @@ const ATTEMPTS_PERIOD = 3000;
 /*async*/ function validate(doc, cancelPromise) {
   attempts = 0;
   return new Promise((resolve, reject) => {
-    function validate() {
+    function val() {
       cancelPromise.then(() => { '...' })
       // TODO:
       if (httpErr) {
         if (attempts++ < MAX_ATTEMPTS) {
-          setTimeout(validate, ATTEMPTS_PERIOD)
+          setTimeout(val, ATTEMPTS_PERIOD)
         }
-        resolve(false);
+        reject(httpErr);
       } else if (ok) {
         resolve(true);
       } else {
-        reject(result);
+        resolve(result);
       }
     }
-    validate();
+    val();
   })
 }
